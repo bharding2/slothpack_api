075e6469b68433e7afacb9d363b6101df9d5ce79
bharding2 <b.harding@gmail.com>
2016-05-17 13:43:25 -0700

    tear this mother down

--- readme.md
+++
@@ -1,3 +0,0 @@
-## slothpack_api
-
-#### This is the server component of the two resouce slothpack client. The server will need a mongo instance running to connect to, but after that `node server.js` and go have fun with the slothpack client
--- test/test_teardown.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const server = require(__dirname + '/../server');
-module.exports = exports = (callback) => {
-  mongoose.connection.db.dropDatabase(() => {
-    mongoose.disconnect(() => {
-      server.close(callback);
-    });
-  });
-};
