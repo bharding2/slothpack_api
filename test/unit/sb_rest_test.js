939e890f94c172626ba189116a739716d39d19b5
bharding2 <b.harding@gmail.com>
2016-05-26 09:37:38 -0700

    getall and delete tests for sbRest

---
+++ test/unit/sb_rest_test.js
@@ -0,0 +1,44 @@
+/* eslint-disable new-cap */
+const angular = require('angular');
+require('angular-mocks');
+
+describe('sb rest services', function() {
+  var $httpBackend;
+
+  beforeEach(angular.mock.module('slothbearApp'));
+  beforeEach(angular.mock.inject(function(_$httpBackend_) {
+    $httpBackend = _$httpBackend_;
+  }));
+
+  it('should return a function', angular.mock.inject(function(sbRest) {
+    expect(typeof sbRest).toBe('function');
+  }));
+
+  it('should get all of the resources', angular.mock.inject(function(sbRest) {
+    $httpBackend.expectGET('http://localhost:5555/api/bears').respond(200, [{ name: 'testy' }]);
+    var bears = [];
+    var errors = [];
+    var testUrl = 'http://localhost:5555/api/bears';
+    var testRes = new sbRest(bears, errors, testUrl);
+    testRes.getAll();
+    $httpBackend.flush();
+    expect(bears.length).toBe(1);
+    expect(bears[0].name).toBe('testy');
+  }));
+
+  it('should create a resource');
+
+  it('should update a resource');
+
+  it('should remove a resource', angular.mock.inject(function(sbRest) {
+    $httpBackend.expectDELETE('http://localhost:5555/api/bears/1').respond(200);
+    var bears = [{ name: 'testy', _id: 1 }];
+    var errors = [];
+    var testUrl = 'http://localhost:5555/api/bears';
+
+    var testRes = new sbRest(bears, errors, testUrl);
+    testRes.remove(bears[0]);
+    $httpBackend.flush();
+    expect(bears.length).toBe(0);
+  }));
+});
